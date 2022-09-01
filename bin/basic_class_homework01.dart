69046d258537bbb7c8ebf8327142a665e7ef4323
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-09-01 21:34:51 +0500

    01

--- bin/basic_class_homework01.dart
+++ bin/basic_class_homework01.dart
@@ -2,4 +2,20 @@
 Create a "Person" class
 */
 
-void main() {}
+void main() {
+  Person s = Person();
+  print(s.name);
+
+}
+class Person{
+String name = '';
+int d = 0;
+Person({ String name = 'Maxkambek', int d = 20}){
+this.name = name;
+this.d = d;
+
+
+}
+
+
+}
