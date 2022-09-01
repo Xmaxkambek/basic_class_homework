d754178c4262d9c2b8a27eb84511678ae80c7616
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-09-01 21:44:55 +0500

    02

--- bin/basic_class_homework02.dart
+++ bin/basic_class_homework02.dart
@@ -3,4 +3,15 @@ Create a "Person" class.
 Create an attribute "name" in the "Person" class.
 */
 
-void main() {}
+void main() {
+  Person s = Person();
+  print(s.name);
+}
+class Person{
+  String name = '';
+
+
+  Person({String name = 'Maxkambek'}){
+
+  }
+}
